8004b38a05409dc49167907646bcd42dc3632fa8
slbin <smpts00@naver.com>
2023-10-23 01:37:25 +0900

    카탈로그 서비스 추가

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'Berlin',100,1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002', 'BAAASaerlin', 110, 4500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003', 'Basdfaerlin', 120, 2500);
---
+++ user-service/src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.userservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .anyRequest().permitAll()
+                )
+                .headers(headers ->
+                        headers.frameOptions(options ->
+                                options.sameOrigin()))
+        ;
+        return http.build();
+    }
+}
